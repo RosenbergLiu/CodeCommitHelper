8e761cd76690ddad1712fb7e6bea96459b0343c2
Rosenberg Xiangyu Liu <rosenbergliu@gmail.com>
2025-03-19 19:30:49 +1100

    x

--- CodeCommitHelper/Views/CreatePullRequestPage.xaml.cs
+++ CodeCommitHelper/Views/CreatePullRequestPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using CodeCommitHelper.ViewModels;
-
+﻿using Amazon.CodeCommit;
+using Amazon.CodeCommit.Model;
+using CodeCommitHelper.ViewModels;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 
 namespace CodeCommitHelper.Views;
@@ -15,5 +17,55 @@ public sealed partial class CreatePullRequestPage : Page
     {
         ViewModel = App.GetService<CreatePullRequestViewModel>();
         InitializeComponent();
+    }
+
+    private async void CreateButton_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            CreateButton.IsEnabled = false;
+
+            var repositoryName = RepositoryInput.Text.Trim();
+            var branchName = BranchNameInput.Text.Trim();
+            var title = TitleInput.Text.Trim();
+
+            var client = new AmazonCodeCommitClient();
+
+            var request = new CreatePullRequestRequest()
+            {
+                Title = title,
+                Targets =
+                [
+                    new Target()
+                    {
+                        RepositoryName = repositoryName,
+                        SourceReference = branchName,
+                        DestinationReference = "dev"
+                    }
+                ],
+                ClientRequestToken = Guid.NewGuid().ToString()
+            };
+
+
+            var response = await client.CreatePullRequestAsync(request);
+
+            OutputText.Text =
+                $"https://ap-southeast-2.console.aws.amazon.com/codesuite/codecommit/repositories/myprosperity-v2/pull-requests/{response.PullRequest.PullRequestId}/details?region=ap-southeast-2";
+        }
+        catch (Exception ex)
+        {
+            OutputText.Text = ex.Message;
+        }
+        finally
+        {
+            CreateButton.IsEnabled = true;
+        }
+    }
+
+    private void ClearButton_Click(object sender, RoutedEventArgs e)
+    {
+        BranchNameInput.Text = string.Empty;
+        RepositoryInput.Text = string.Empty;
+        TitleInput.Text = string.Empty;
     }
 }
